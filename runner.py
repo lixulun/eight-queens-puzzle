4172f9bb1c92261dde6f7d6b2f69d054eae76020
lixulun <lixulun@126.com>
2025-12-19 11:15:05 +0800

    crystal 版本

--- runner.py
+++ runner.py
@@ -8,19 +8,25 @@ def run_python_version():
     subprocess.run("python3 solve.py".split())
 
 
+def run_crystal_version():
+    subprocess.run("crystal solve.cr".split())
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-x",
         "--program",
         required=True,
-        choices={"python"},
+        choices={"python", "crystal"},
         help="指定执行哪个版本",
     )
 
     args = parser.parse_args()
     if args.program == "python":
         run_python_version()
+    elif args.program == "crystal":
+        run_crystal_version()
 
 
 if __name__ == "__main__":
