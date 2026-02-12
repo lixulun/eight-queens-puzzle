3d3b93662648844a5c93cd6bd8d3f43950767c65
lixulun <lixulun@126.com>
2026-02-12 15:33:30 +0800

    refactor ruby version

--- ruby/solve.rb
+++ ruby/solve.rb
@@ -1,56 +1,48 @@
+# frozen_string_literal: true
+
 QUEEN = "\u2655"
 
 def print_solution(board)
   n = board.size
   n.times do |row|
-    n.times { print "+---" }
-    puts "+"
-    board.map { |cell| print "| " + (cell == row ? QUEEN : " ") + " " }
-    puts "|"
+    n.times { print '+---' }
+    puts '+'
+    board.map { |cell| print "| #{cell == row ? QUEEN : ' '} " }
+    puts '|'
   end
-  n.times { print "+---" }
-  puts "+"
+  n.times { print '+---' }
+  puts '+'
 end
 
-def is_place_ok(board, row, col)
+def place_ok?(board, row, col)
   slash = row - col
   divide = row + col
   n = board.size
   n.times do |i|
     n.times do |j|
-      if i == col && j == row
-        next
-      end
-      if board[i] == j
-        if i == col || j == row
-          return false
-        end
-        if j - i == slash || j + i == divide
-          return false
-        end
-      end
+      next if i == col && j == row
+
+      return false if board[i] == j && (i == col || j == row)
+      return false if board[i] == j && (j - i == slash || j + i == divide)
+
     end
   end
   true
 end
 
 def solve(board, col)
-  if col >= board.size
-    return board
-  else
-    board.size.times do |row|
-      if is_place_ok(board, row, col)
-        board[col] = row
-        solved = solve(board, col + 1)
-        if solved.size > 0
-          return solved
-        else
-          board[col] = -1
-        end
-      end
-    end
-    []
+  return board if col >= board.size
+
+  board.size.times do |row|
+    next unless place_ok?(board, row, col)
+
+    board[col] = row
+    solved = solve(board, col + 1)
+    return solved unless solved.empty?
+
+    board[col] = -1
   end
+  []
 end
 
-print_solution(solve([-1,-1,-1,-1,-1,-1,-1,-1], 0))
+print_solution(solve([-1, -1, -1, -1, -1, -1, -1, -1], 0))
